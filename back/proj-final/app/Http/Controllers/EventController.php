a8c3f2d68f452c73cefd3373d0e88b70bf45f018
Martí <msolert@fp.insjoaquimmir.cat>
2024-04-30 15:10:39 +0200

    Event controller wip

--- back/proj-final/app/Http/Controllers/EventController.php
+++ back/proj-final/app/Http/Controllers/EventController.php
@@ -30,6 +30,44 @@ class EventController extends Controller
     }
     public function create()
     {
-        return view("places.create");
+        // Validar dades del formulari
+        $validatedData = $request->validated();
+        
+        // Obtenir dades del formulari
+        $name        = $request->get('name');
+        $description = $request->get('description');
+        $upload      = $request->file('upload');
+        $latitude    = $request->get('latitude');
+        $longitude   = $request->get('longitude');
+        $visibility  = $request->get('visibility');
+
+        // Desar fitxer al disc i inserir dades a BD
+        $file = new File();
+        $fileOk = $file->diskSave($upload);
+
+        if ($fileOk) {
+            // Desar dades a BD
+            Log::debug("Saving place at DB...");
+            $place = Place::create([
+                'name'          => $name,
+                'description'   => $description,
+                'file_id'       => $file->id,
+                'latitude'      => $latitude,
+                'longitude'     => $longitude,
+                'author_id'     => auth()->user()->id,
+                'visibility_id' => $visibility,
+            ]);
+            Log::debug("DB storage OK");
+            // Patró PRG amb missatge d'èxit
+            return redirect()->route('places.show', $place)
+                ->with('success', __(':resource successfully saved', [
+                    'resource' => __('Place')
+                ]));
+        } else {
+            Log::debug("Disk storage FAILS");
+            // Patró PRG amb missatge d'error
+            return redirect()->route("places.create")
+                ->with('error', __('ERROR uploading file'));
+        }
     }
 }
