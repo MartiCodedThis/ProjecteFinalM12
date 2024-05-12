ef21e45b07819a227f88d569db6c655f71cdb338
mriuse <mriuse@fp.insjoaquimmir.cat>
2024-05-13 00:32:59 +0200

    WIP profile

--- front/src/components/widgets/BrancaSelector.jsx
+++ front/src/components/widgets/BrancaSelector.jsx
@@ -10,19 +10,13 @@ const BrancaSelector = (props) => {
         })
     }
   return (
-    <div>
-        <p>Selecciona la teva branca</p>
-        <div>
-            <button onClick={()=>selectBranca(0)} >Follets</button>
-        </div>
-        <div>
-            <button onClick={()=>selectBranca(1)}>Llobatons</button>
-        </div>
-        <div>
-            <button onClick={()=>selectBranca(2)}>Puputs</button>
-        </div>
-        <div>
-            <button onClick={()=>selectBranca(3)}>Ràngers</button>
+    <div className='mb-4'>
+        <p>Selecciona la teva branca:</p>
+        <div className="flex flex-wrap *:min-w-32 gap-2 my-4 *:rounded-xl *:shadow-md *:px-4 *:py-2 *:font-bold">
+            <button className="bg-follets text-apptext" onClick={()=>selectBranca(0)} >Follets</button>
+            <button className="bg-llobatons text-white" onClick={()=>selectBranca(1)}>Llobatons</button>
+            <button className="bg-puputs text-white" onClick={()=>selectBranca(2)}>Puputs</button>
+            <button className="bg-rangers text-apptext" onClick={()=>selectBranca(3)}>Ràngers</button>
         </div>  
     </div>
   )
--- front/src/pages/Profile.jsx
+++ front/src/pages/Profile.jsx
@@ -3,88 +3,95 @@ import useServicesContext from '../hooks/useServicesContext'
 import BrancaSelector from '../components/widgets/BrancaSelector'
 
 export const Profile = () => {
-    const {services: {authService, sessionService}} = useServicesContext()
+    const { services: { authService, sessionService } } = useServicesContext()
     const [user, setUser] = useState()
     const [unauthorizedUsers, setUnauthorizedUsers] = useState()
     const [authorizedUsers, setAuthorizedUsers] = useState()
     const [refresh, setRefresh] = useState()
 
     let token = sessionService.getToken()
-    useEffect(()=>{
+    useEffect(() => {
         setRefresh(false)
-        authService.getUser(token).then((u)=>{
+        authService.getUser(token).then((u) => {
             setUser(u.user)
-            if(u.role == 1){
-                authService.listUnauthUsers(token).then((e)=>{
+            if (u.role == 1) {
+                authService.listUnauthUsers(token).then((e) => {
                     setUnauthorizedUsers(e.userlist)
                 })
-                authService.listAuthUsers(token).then((e)=>{
+                authService.listAuthUsers(token).then((e) => {
                     setAuthorizedUsers(e.userlist)
                 })
             }
         })
-    },[refresh])
+    }, [refresh])
 
     const handleAuthorize = async (user) => {
-        authService.authorize(token, user.id).then(()=>{
+        authService.authorize(token, user.id).then(() => {
             setRefresh(true)
         })
     }
     const handleUnauthorize = async (user) => {
-        authService.unauthorize(token, user.d).then(()=>{
+        authService.unauthorize(token, user.d).then(() => {
             setRefresh(true)
         })
     }
-  return (
-    <div>
-        {user ? 
-        <>
-            <h2> {user.name} </h2>
-            <p> {user.email} </p>
-            <p> Branca: {user.branca >= 0 ? user.branca : <> No tens cap branca assignada </>}</p>
-            <BrancaSelector refresh={setRefresh}></BrancaSelector>
-                
-            {user.carrec ? 
-            <p> {user.carrec} </p>
-            : <></>}
-            {user.role_id == 1 ? 
-            <div>
-                <div>
-                    <h2>Authorize Users</h2>
-                    {unauthorizedUsers ?   
-                    unauthorizedUsers.map((user)=>{
-                        if(user.role_id != 1){
-                            return(
-                                <>{user.name}  {user.email}
-                                    <button onClick={()=>handleAuthorize(user)}> Authorize </button>
-                                    <br />
-                                </>
-                            )
-                        }
-                    })     
-                    : <></>}
-
-                </div>
-                <div>
-                    <h2>Unauthorize Users</h2>
-                    {authorizedUsers ?   
-                    authorizedUsers.map((user)=>{
-                        if(user.role_id != 1){
-                            return(
-                                <>{user.name}  {user.email}
-                                    <button onClick={()=>handleUnauthorize(user)}> Unauthorize </button>
-                                    <br />
-                                </>
-                            )
-                        }
-                    })     
-                    : <></>}
-                </div>
+    return (
+        <div className="mb-12">
+            <h2 className="text-3xl md:text-5xl font-bold mb-4">Perfil d'usuari</h2>
+            <hr className="border-appsep mb-4"></hr>
+            <div className='flex flex-col bg-appfg justify-center rounded-2xl shadow-xl p-8 md:p-16 my-8 sm:my-16 mx-0 lg:mx-10'>
+                {user ?
+                    <>
+                        <h3 className='font-bold text-apptext2'>Nom d'usuari</h3><p className='mb-4'>{user.name}</p>
+                        <h3 className='font-bold text-apptext2'>Adreça de correu:</h3><p className='mb-4'>{user.email}</p>
+                        <h3 className='font-bold text-apptext2'>Branca:</h3><p className='mb-4'>{user.branca >= 0 ? user.branca : <> No tens cap branca assignada </>}</p>
+                        <BrancaSelector refresh={setRefresh}></BrancaSelector>
+                        {user.carrec ?
+                            <p> {user.carrec} </p>
+                        : <></>}
+                        {user.role_id == 1 ?
+                            <>
+                                <hr className='border-appsep2 mb-8'></hr>
+                                <h3 className="font-bold text-apptext2 mb-4">Opcions d'administrador</h3>
+                                <div className='mb-4'>
+                                    <h4 className='font-bold text-sm text-apptext2'>Usuaris no autoritzats</h4>
+                                    {unauthorizedUsers ?
+                                        unauthorizedUsers.map((user) => {
+                                            if (user.role_id != 1) {
+                                                return (
+                                                    <>{user.name}  {user.email}
+                                                        <button onClick={() => handleAuthorize(user)}>Autoritzar</button>
+                                                        <br />
+                                                    </>
+                                                )
+                                            }
+                                        })
+                                    : <>
+                                        <p>No hi ha usuaris per autoritzar.</p>
+                                    </>}
+                                </div>
+                                <div className='mb-4'>
+                                    <h4 className='font-bold text-sm text-apptext2'>Usuaris autoritzats</h4>
+                                    {authorizedUsers ?
+                                        authorizedUsers.map((user) => {
+                                            if (user.role_id != 1) {
+                                                return (
+                                                    <>{user.name}  {user.email}
+                                                        <button onClick={() => handleUnauthorize(user)}>Desautoritzar</button>
+                                                        <br />
+                                                    </>
+                                                )
+                                            }
+                                        })
+                                        : <>
+                                            <p>No hi ha usuaris per desautoritzar.</p>
+                                        </>}
+                                </div>
+                            </>
+                        : <></>}
+                    </>
+                : <><p>Carregant...</p></>}
             </div>
-            : <></> }
-        </>
-        : <>Carregant ...</>}
-        
-    </div>
-  )
+        </div>
+    )
 }
