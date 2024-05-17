241c4141ba469a98e73bf453fd8636cca4520123
Martí <msolert@fp.insjoaquimmir.cat>
2024-05-17 14:41:29 +0200

    Task responsibles problems solved

--- front/src/pages/tasks/TaskShow.jsx
+++ front/src/pages/tasks/TaskShow.jsx
@@ -27,6 +27,7 @@ const TaskShow = () => {
     taskService.get(token, params.id).then((response) => {
       setTask(response.task);
       setResponsibles(response.users);
+      console.log(response)
       if (response.branca.length > 0) {
         let aux = [];
         response.branca.map((b) => {
